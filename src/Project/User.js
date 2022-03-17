33b2e50ed44b8ad6cb1c829313155b959e884f9b
Arbab <arbabanjum0347@gmail.com>
2022-03-17 16:28:57 +0500

    Error Handling

--- src/Project/User.js
+++ src/Project/User.js
@@ -4,20 +4,32 @@ import React,{useState,useEffect} from 'react'
 function User() {
 
     const [Users, setUsers] = useState([]);
+    const [Loading, setLoading] = useState(true);
     const getData = async()=>{
-        const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-        setUsers(await response.json());
+        try{
+            setLoading(!true);
+            const response = await fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=30');
+            setUsers(await response.json());
+        }
+        catch(err){
+            console.log("My Error",err);
+        }
+        
     }
     useEffect(() => {
         getData();
     }, []);
+
+    if(Loading){
+        return <h1>Loading.....</h1>
+    }
   return (
     <>
     <h1 className='text-center'>All Users</h1>
     <div className="container-fluid">
         <div className="row">
             {Users.map((data)=>{
-                    return<div className="col-12 col-md-4 col-lg-3" key={data.id}>
+                    return<div className="col-12 col-sm-6 col-md-4 col-lg-3 mt-3" key={data.id}>
                             <div className="card">
                                 <img src={data.url} className="card-img-top" alt="..."/>
                                 <div className="card-body">
