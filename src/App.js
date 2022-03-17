817101b93854843ebfd977d52f589b4bb940047b
Arbab <arbabanjum0347@gmail.com>
2022-03-17 15:39:53 +0500

    Use Effect Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import UseState from "./Hooks/UseState";
+// import UseEffect from "./Hooks/UseEffect";
 
-import ReactForm from "./Forms/ReactForm";
+// import ReactForm from "./Forms/ReactForm";
 
 function App() {
   return (
     <div className="App">
       {/* <UseState/> */}
-      <ReactForm/>
+      {/* <UseEffect/> */}
+      {/* <ReactForm/> */}
     </div>
   );
 }
--- src/Hooks/UseEffect.js
+++ src/Hooks/UseEffect.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 export default function UseEffect() {
+  const [Count, setCount] = useState(0);
+  const [Width, setWidth] = useState(window.screen.width);
+  useEffect(() => {
+    if (Count>=1) {
+      document.title=`Chats(${Count})`;
+    }
+    else{
+      document.title=`Chats`;
+    }
+   
+    return () => {
+     console.log("Cleanup");
+    };
+  },[Count]);
+
+  const actualWidth = ()=>{
+    setWidth(window.innerWidth);
+  }
+    useEffect(() => {
+      window.addEventListener("resize",actualWidth);
+      return()=>{
+              window.removeEventListener("resize",actualWidth);
+      }
+    });
+
   return (
     <>
+    <h1>Chat Count</h1>
+    <h1>{Count}</h1>
+    <button onClick={()=>setCount(Count+1)}>Click</button>
+    <br /><br />
+    <h1>Window Size</h1>
+    <h1>{Width}px</h1>
     </>
   )
 }
